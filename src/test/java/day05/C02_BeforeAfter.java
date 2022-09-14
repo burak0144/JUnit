ec056dc2e100397c36dff5935a650d11834b1a66
burak <karanfilkaranfil822@gmail.com>
2022-09-14 22:29:34 +0300

    "9.14.22"

--- src/test/java/day05/C02_BeforeAfter.java
+++ src/test/java/day05/C02_BeforeAfter.java
@@ -1,5 +1,52 @@
 package day05;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class C02_BeforeAfter {
-    public void
+    WebDriver driver;
+    @Before
+    //Before notasyonunu her testen onc calisir
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+    @Test
+    public void method1(){
+        //1 defa before methodu calisir
+        driver.get("https://amazon.com");
+        //1 defa da after methodu calisir
+    }
+    @Test
+    public void method2(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://techproeducation.com");
+
+    }
+    @Test
+    public void method3(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://hepsiburada.com");
+
+    }
+    @After
+    //After notasyonu her testten sonra calisir
+    public void tearDown(){
+       driver.close();
+    }
 }
