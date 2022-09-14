202101a92266fc8817ed14f4b28f52b34989bf3c
burak <karanfilkaranfil822@gmail.com>
2022-09-14 22:57:47 +0300

    "9.14.22"

--- src/test/java/day05/C03_BeforeClassAfterClass.java
+++ src/test/java/day05/C03_BeforeClassAfterClass.java
@@ -5,6 +5,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
@@ -15,16 +16,10 @@ public class C03_BeforeClassAfterClass {
     @BeforeClass
     public static void setUp(){
         WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-
     }
-
-    @AfterClass
-    public static void tearDown(){
-        driver.close();
-    }
-
     @Test
     public void method1(){
         driver.get("https://amazon.com");
@@ -36,5 +31,9 @@ public class C03_BeforeClassAfterClass {
     @Test
     public void method3(){
         driver.get("https://hepsiburada.com");
+    }
+    @AfterClass
+    public static void tearDown(){
+        driver.close();
     }
 }
