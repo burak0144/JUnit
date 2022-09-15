15c163c90175cda4f33f4a41f0ecccf4a3eb949a
burak <karanfilkaranfil822@gmail.com>
2022-09-15 21:07:27 +0300

    "9.14.22"

---
+++ src/test/java/day06/RadioButton.java
@@ -0,0 +1,46 @@
+package day06;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class RadioButton {
+    WebDriver driver;
+//Gerekli yapiyi olusturun ve aşağıdaki görevi tamamlayın.
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+//https://www.facebook.com adresine gidin
+    @Test
+    public void test(){
+        driver.get("https://www.facebook.com");
+
+//“Create an Account” button’una basin
+        driver.findElement(By.xpath("(//a[@role='button'])[2]")).click();
+//“radio buttons” elementlerini locate edin
+  WebElement femaleButtonWE= driver.findElement(By.xpath("//input[@value='1']"));
+  WebElement maleButtonWE= driver.findElement(By.xpath("//input[@value='2']"));
+  WebElement customButtonWE=driver.findElement(By.xpath("//input[@value='-1']"));
+//Secili degilse cinsiyet butonundan size uygun olani secin
+        if(!maleButtonWE.isSelected()){
+            maleButtonWE.click();
+        }
+
+
+} }
