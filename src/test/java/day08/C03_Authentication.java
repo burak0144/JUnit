f8c003c482481fea91d9fcfa5ec758fa8cace4f0
burak <karanfilkaranfil822@gmail.com>
2022-09-17 19:42:39 +0300

    "9.17.22"

--- src/test/java/day08/C03_Authentication.java
+++ src/test/java/day08/C03_Authentication.java
@@ -2,8 +2,10 @@ package day08;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -22,16 +24,20 @@ public class C03_Authentication {
 
     @Test
     public void test() throws InterruptedException {
-        //Bir class olusturun : BasicAuthentication
+
         //https://the-internet.herokuapp.com/basic_auth sayfasina gidin
         //asagidaki yontem ve test datalarini kullanarak authentication’i yapin
         //Html komutu : https://username:password@URL
         //Username    : admin
         //password    : admin
         //Basarili sekilde sayfaya girildigini dogrulayin
+        driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+        String actualAnaSayfa=driver.findElement(By.xpath("//h3[text()='Basic Auth']")).getText();
+        String expectedMesaj="Basic Auth";
+        Assert.assertEquals(actualAnaSayfa, expectedMesaj);
     }
     @After
     public void tearDown() {
-        driver.close();
+        //driver.close();
     }
 }
