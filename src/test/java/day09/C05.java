020ece4e7956ec638ddc66568dd19d9361981124
burak <karanfilkaranfil822@gmail.com>
2022-09-19 23:29:00 +0300

    "9.19.22"

--- src/test/java/day09/C05.java
+++ src/test/java/day09/C05.java
@@ -8,6 +8,6 @@ sayfadaki iframe sayısını bulunuz.
 ilk iframe'deki (Youtube) play butonuna tıklayınız.
 ilk iframe'den çıkıp ana sayfaya dönünüz
 ikinci iframe'deki (Jmeter Made Easy) linke (https://www.guru99.com/live-selenium-project.html)
-tıklayınız
+tıklayınız.
      */
 }
