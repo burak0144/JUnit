ff8b07c3b25df1d0c767fad5af03556e2595ae71
burak <karanfilkaranfil822@gmail.com>
2022-09-21 22:16:03 +0300

    "9.19.22"

---
+++ src/test/java/day11/C04_Odev.java
@@ -0,0 +1,8 @@
+package day11;
+
+public class C04_Odev {
+  // Bir Class olusturalim KeyboardActions2
+  // https://html.com/tags/iframe/ sayfasina gidelim  3- video’yu gorecek kadar asagi inin
+  // videoyu izlemek icin Play tusuna basin
+  // videoyu calistirdiginizi test edin
+}
