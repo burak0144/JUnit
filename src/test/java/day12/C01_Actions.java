86649556dcf2b14cb4edfee83b7d3368566b9eeb
burak <karanfilkaranfil822@gmail.com>
2023-03-28 15:39:19 +0300

    28.03.23

--- src/test/java/day12/C01_Actions.java
+++ src/test/java/day12/C01_Actions.java
@@ -2,7 +2,9 @@ package day12;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import utilies.TestBaseBeforeAfter;
 
@@ -15,6 +17,8 @@ public class C01_Actions extends TestBaseBeforeAfter {
         Actions actions=new Actions(driver);
 driver.get("https://www.automationexercise.com/");
 driver.findElement(By.xpath("//a[@href='/products']")).click();
+
+
 actions.sendKeys(Keys.PAGE_DOWN).perform();
 driver.findElement(By.xpath("//a[@href='/product_details/1']")).click();
     }
